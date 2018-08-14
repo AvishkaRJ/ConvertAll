29ba76d93d9e2603b87ed0f146314d6c49ef3f30
AvishkaRJ <avishkaranditha@gmail.com>
2018-08-14 09:38:04 +0530

    Version 1

---
+++ convert_all.py
@@ -0,0 +1,10 @@
+
+import os,sys
+folder = r'C:\Users\Default\Desktop'
+
+for filename in os.listdir(folder):
+       infilename = os.path.join(folder,filename)
+       if not os.path.isfile(infilename): continue
+       oldbase = os.path.splitext(filename)
+       newname = infilename.replace('.der', '.txt')
+       output = os.rename(infilename, newname)
